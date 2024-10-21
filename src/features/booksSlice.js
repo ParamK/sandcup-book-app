31bd317063e6af32684855eaf28021f48b48345d
ParamK <paramkatkoria555@gmail.com>
2024-10-21 09:12:39 +0530

    extra comments removed

--- src/features/booksSlice.js
+++ src/features/booksSlice.js
@@ -1,12 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import booksData from '../data/books.json';
 
-// Load the initial reading list from localStorage, or default to an empty array
 const storedReadingList = JSON.parse(localStorage.getItem('readingList')) || [];
 
 const initialState = {
     books: booksData,
-    readingList: storedReadingList, // Use the stored reading list on page load
+    readingList: storedReadingList,
 };
 
 const booksSlice = createSlice({
@@ -19,13 +18,11 @@ const booksSlice = createSlice({
 
             if (!alreadyInList) {
                 state.readingList.push(book);
-                // Update localStorage
                 localStorage.setItem('readingList', JSON.stringify(state.readingList));
             }
         },
         removeFromReadingList: (state, action) => {
             state.readingList = state.readingList.filter((book) => book.id !== action.payload);
-            // Update localStorage
             localStorage.setItem('readingList', JSON.stringify(state.readingList));
         },
     },
